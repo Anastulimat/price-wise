88d721a4a3c748679e0166a058a225d897bc3fb5
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-10-23 22:29:39 +0200

    feat: add images carousel to hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Image from "next/image";
 import SearchBar from "@/components/SearchBar";
+import HeroCarousel from "@/components/HeroCarousel";
 
 const Home = () => {
     return (
         <>
-            <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+            <section className="px-6 md:px-20 py-20">
                 <div className="flex max-xl:flex-col gap-16">
                     <div className="flex flex-col justify-center">
                         <p className="small-text">
@@ -30,7 +31,7 @@ const Home = () => {
                         <SearchBar />
                     </div>
 
-                    HeroCarousel
+                    <HeroCarousel />
                 </div>
             </section>
             
